3c2ffdadc551419947cc610330b54d1b44b7dc4d
lirockk <arthurdemurger26@gmail.com>
2022-06-23 19:26:33 +0200

    Change the fd for errors

--- srcs/error.c
+++ srcs/error.c
@@ -6,7 +6,7 @@
 /*   By: ademurge <ademurge@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/11 17:06:27 by ademurge          #+#    #+#             */
-/*   Updated: 2022/06/23 19:03:17 by ademurge         ###   ########.fr       */
+/*   Updated: 2022/06/23 19:15:30 by ademurge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,6 @@ void	ft_error(char *type, int is_perror, t_arg *args)
 	if (type && is_perror)
 		perror(type);
 	else if (type)
-		write (1, type, ft_strlen(type));
+		write (STDERR_FILENO, type, ft_strlen(type));
 	exit(EXIT_FAILURE);
 }
